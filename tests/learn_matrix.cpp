d2c158c4ee11a2606567a809b47a6d05e5f9fc82
Nicolas Canac <ncanac@kev.ps.uci.edu>
2015-08-15 18:22:26 -0700

    some more tests with matrix class

--- tests/learn_matrix.cpp
+++ tests/learn_matrix.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 #include <matrix_impl.hpp>
+#include <macros.hpp>
 
 int main()
 {
@@ -9,17 +10,31 @@ int main()
     int n = 3;
     int m = 4;
     Math::Matrix<double> mat(n, m, 0);
+    check(mat.rows() == n, "");
+    check(mat.cols() == m, "");
+
+    // Initialize a row vector and its transpose
     std::vector<double> vec {2, 3, -1};
-    // Initialize a row vector
-    Math::Matrix<double> mat(vec, false);
-    Math::Matrix<double> matT = mat.getTranspose();
+    Math::Matrix<double> mat2(vec, false);
+    Math::Matrix<double> mat2T = mat2.getTranspose();
+    check(mat2(0, 2) == mat2T(2, 0), "");
 
-    std::cout << mat(0, 2) << std::endl;
-    std::cout << matT(2, 0) << std::endl;
-
+    // Multiply mat with matT and store result in res
     Math::Matrix<double> res;
-    Math::Matrix<double>::multiplyMatrices(mat, matT, &res);
-    res.writeIntoTextFile("result.txt");
+    Math::Matrix<double>::multiplyMatrices(mat2, mat2T, &res);
+    check(res(0,0) == 14, "");
+
+    // Initialize an empty matrix
+    Math::Matrix<double>mat3;
+    // Resize a matrix to be n x m and contain all zeros
+    mat3.resize(n, m, 0);
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j < m; ++j)
+        {
+            check(mat3(0, 0) == 0, "");
+        }
+    }
 
     return 0;
 }
