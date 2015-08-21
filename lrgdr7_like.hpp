6a08a7ba8d25c53d7b0829ddbf7c8e4985d77d88
Nicolas Canac <ncanac@kev.ps.uci.edu>
2015-08-20 18:16:07 -0700

    added b-spline for calculating P_nw

--- lrgdr7_like.hpp
+++ lrgdr7_like.hpp
@@ -4,11 +4,16 @@
 
 #include <string>
 #include <fstream>
+#include <cmath>
 
 #include <macros.hpp>
 #include <cosmo.hpp>
 #include <likelihood_function.hpp>
 #include <matrix_impl.hpp>
+#include <cubic_spline.hpp>
+
+#include <gsl/gsl_bspline.h>
+#include <gsl/gsl_multifit.h>
 
 class LRGDR7Likelihood : public Math::LikelihoodFunction
 {
@@ -225,7 +230,35 @@ public:
         for(int i = 0; i < k_size_; ++i)
             P_lin(i, 0) = P_lin_function.evaluate(k_[i]);
 
-        // TODO: I think rescaling goes here. Use fiducial model.
+        // Initialize P_nw (linear power spectrum with "no wiggles") based on spline method in BR09
+        // Procedure:
+        // -Fit a cubic b-spline to Plin*k^1.5
+        // -Eight equally spaced nodes starting at k = 0.0175 Mpc^-1 to 0.262 Mpc^-1. In log space?
+        // -One additional node at k = 0.0007 Mpc^-1
+        // TODO: Don't use Math::CubicSpline... not the same as b-spline. Use b-spline code from original LRG code.
+        // Temporary arrays to pass to function dopksmoothbspline_
+        double kvals[k_size_];
+        double lnP_lin[k_size_];
+        double lnP_nw[k_size_];
+        // initialize arrays with values in k_ and P_lin
+        for(int i = 0; i < k_size_; ++i)
+        {
+            kvals[i] = k_[i];
+            lnP_lin[i] = log(P_lin(i,0));
+        }
+        dopksmoothbspline_(kvals, lnP_lin, lnP_nw, k_size_);
+        // Store result of smoothing in P_nw
+        Math::Matrix<double> P_nw(k_size_, 1, 0);
+        for(int i = 0; i < k_size_; ++i)
+            P_nw(i, 0) = exp(lnP_nw[i]);
+
+        // Writing P_lin and P_nw to file
+        std::ofstream outTest("test.txt");
+        for(int i = 0; i < k_size_; ++i)
+            outTest << k_[i] << " " << P_lin(i,0) << " " << P_nw(i,0) << std::endl;
+        outTest.close();
+
+        // TODO: I think some rescaling goes here. Use fiducial model.
 
         output_screen("Checkpoint 1" << std::endl);
 
@@ -304,6 +337,115 @@ public:
     }
 
 private:
+    // Copied from BR09 LRG likelihood code, which was copied from http://www.gnu.org/software/gsl/manual/html_node/Basis-Splines.html
+    // Inputs:
+    // kvals is an array containing the values of k to evaluate the power spectra at
+    // lnpklinear is the natural logarithm of the linear power spectrum
+    // npts is the length of the input (and output) arrays
+    // Outputs:
+    // lnpksmooth is the natural logarithm of the smoothed linear power spectrum
+    void dopksmoothbspline_(double *kvals, double *lnpklinear, double *lnpksmooth, int npts)
+    {
+	    double kmaxsuppress = 0.01*0.7;
+	    size_t n, ncoeffs, nbreak;
+	    gsl_bspline_workspace *bw;
+	    gsl_vector *B;
+	    gsl_vector *c, *w, *x, *y;
+	    gsl_matrix *X, *cov;
+	    gsl_multifit_linear_workspace *mw;
+	    double deltak,lastk;
+	    int i,j,countkeep;
+
+	    nbreak = 9;
+	    gsl_vector *mybreaks = gsl_vector_alloc(nbreak);
+	    gsl_vector_set(mybreaks,0,(0.001*0.7));
+	    gsl_vector_set(mybreaks,1,(0.025*0.7));
+	    gsl_vector_set(mybreaks,2,(0.075*0.7));
+	    gsl_vector_set(mybreaks,3,(0.125*0.7));
+	    gsl_vector_set(mybreaks,4,(0.175*0.7));
+	    gsl_vector_set(mybreaks,5,(0.225*0.7));
+	    gsl_vector_set(mybreaks,6,(0.275*0.7));
+	    gsl_vector_set(mybreaks,7,(0.325*0.7));
+	    gsl_vector_set(mybreaks,8,(0.375*0.7));
+
+	    countkeep = 0;
+	    for(i=0;i<npts;i++)  {
+	    	if((kvals[i]) >= gsl_vector_get(mybreaks,0) && (kvals[i]) <= gsl_vector_get(mybreaks,nbreak-1)) {
+	    		countkeep += 1;
+	    		}
+	    	}
+	    n = countkeep;
+	    ncoeffs = nbreak + 2;
+
+	    /* allocate a cubic bspline workspace (k = 4) */
+	    bw = gsl_bspline_alloc(4, nbreak);
+	    B = gsl_vector_alloc(ncoeffs);     
+	    x = gsl_vector_alloc(n);
+	    y = gsl_vector_alloc(n);
+	    X = gsl_matrix_alloc(n, ncoeffs);
+	    c = gsl_vector_alloc(ncoeffs);
+	    w = gsl_vector_alloc(n);
+	    cov = gsl_matrix_alloc(ncoeffs, ncoeffs);
+	    mw = gsl_multifit_linear_alloc(n, ncoeffs);
+	    i=0;
+	    for(j=0;j<npts;j++)  {
+	    	if((kvals[j]) >= gsl_vector_get(mybreaks,0) && (kvals[j]) <= gsl_vector_get(mybreaks,nbreak-1)) {
+	    		gsl_vector_set(x,i,(kvals[j]));
+	    		gsl_vector_set(y,i,exp(lnpklinear[j])*pow(kvals[j],1.5));
+	    		if(j>0)  {
+	    			deltak = kvals[j] - kvals[j-1];
+	    			}
+	    		else {
+	    			deltak = kvals[0];
+	    			if(kvals[1] - kvals[0] < deltak)  {
+	    				deltak = kvals[1]-kvals[0];
+	    				}
+	    			}
+	    		gsl_vector_set(w,i,deltak);
+	    		i+=1;
+	    		}
+	    	}
+	    gsl_bspline_knots(mybreaks,bw);
+	    for(i=0;i<n;i++)  {
+	    	double xi = gsl_vector_get(x,i);
+	    	gsl_bspline_eval(xi,B,bw);
+	    	for(j=0;j<ncoeffs;j++)  {
+	    		double Bj = gsl_vector_get(B,j);
+	    		gsl_matrix_set(X,i,j,Bj);
+	    		}
+	    	}
+	    //do fit
+	    double yi,yierr,chisq;
+	    gsl_multifit_wlinear(X,w,y,c,cov,&chisq,mw);
+	    i = 0;
+	    for(j=0;j<npts;j++)  {
+	    	if((kvals[j]) >= gsl_vector_get(mybreaks,0) && (kvals[j]) <= gsl_vector_get(mybreaks,nbreak-1)) {
+	    		gsl_bspline_eval(gsl_vector_get(x,i),B,bw);
+	    		gsl_multifit_linear_est(B,c,cov,&yi,&yierr);
+	    		lnpksmooth[j] = log(yi*pow(kvals[j],-1.5));
+	    		i += 1;
+	    		}
+	    	else {
+	    		lnpksmooth[j] = lnpklinear[j];
+	    		}
+	    	//spline is wacky at small k -- suppress difference at k < 0.01
+	    	if(kvals[j] < kmaxsuppress)  {
+	    		lnpksmooth[j] = lnpklinear[j];
+	    		}
+	    	}
+	    //assert(i==n);
+	    gsl_bspline_free(bw);
+	    gsl_vector_free(B);
+	    gsl_vector_free(x);
+	    gsl_vector_free(y);
+	    gsl_vector_free(mybreaks);
+	    gsl_matrix_free(X);
+	    gsl_vector_free(c);
+	    gsl_vector_free(w);
+	    gsl_matrix_free(cov);
+	    gsl_multifit_linear_free(mw);
+    }
+
     int lMax_;
     Cosmo* cosmo_;
 
