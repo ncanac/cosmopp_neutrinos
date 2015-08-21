a57a6c7b9d365583feaead9d99c4910de7fb485e
Nicolas Canac <ncanac@kev.ps.uci.edu>
2015-08-20 18:12:02 -0700

    Added getR_NL. Not sure if working properly yet.

--- cosmo.hpp
+++ cosmo.hpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <cmb.hpp>
 #include <class.h>
 
@@ -50,5 +51,55 @@ public:
         free(pvecback);
         
         return pvecback[br_->index_bg_H];
+    }
+
+    double getR_NL()
+    {
+        double z, k_nl, k;
+        double* pvecback;
+        int junk;
+        double r_nl;
+        int index_tau; // Think I can get rid of this and replace last instance with nl_->tau[index_tau]
+
+        output_screen("Checkpoint 1" << std::endl);
+
+        pt_->k_max_for_pk = kMax_;
+        nl_->method = nl_halofit;
+        pr_->halofit_dz = 0.1;
+        pr_->halofit_min_k_nonlinear = 0.0035;
+        pr_->halofit_sigma_precision = 0.05;
+
+        nonlinear_free(nl_);
+        nonlinear_init(pr_, br_, th_, pt_, pm_, nl_);
+
+        //nl_->method = nl_halofit;
+        output_screen("Checkpoint 1.1" << std::endl);
+        z = 0.0;
+        nonlinear_k_nl_at_z(br_, nl_, z, &k_nl);
+
+        output_screen("Checkpoint 2" << std::endl);
+
+        pvecback = (double*) calloc(br_->bg_size,sizeof(double));
+
+        for(index_tau = 0; index_tau < nl_->tau_size; ++index_tau)
+        {
+            background_at_tau(br_, nl_->tau[index_tau], br_->short_info,
+                                br_->inter_normal, &junk, pvecback);
+        }
+
+        output_screen("Checkpoint 3" << std::endl);
+
+        z = br_->a_today/pvecback[br_->index_bg_a]-1.0;
+        
+        std::ofstream out("test_nonlinear.txt");
+        for(int index_k = 0; index_k < nl_->k_size; ++index_k)
+        {
+            k = nl_->k[index_k];
+            r_nl = nl_->nl_corr_density[index_tau * nl_->k_size + index_k];
+            out << z << " " << k << " " << r_nl << std::endl;
+        }
+        out.close();
+        free(pvecback);
+        return 0;
     }
 };
