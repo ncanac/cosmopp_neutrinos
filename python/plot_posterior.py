aeecf741007cc2dc6a7741f0b5950dc80dff8f2d
Nicolas Canac <ncanac@uci.edu>
2016-01-13 18:37:58 -0800

    worked on plotting

--- python/plot_posterior.py
+++ python/plot_posterior.py
@@ -1,17 +1,21 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import sys
+from matplotlib.patches import Ellipse
+from scipy.interpolate import griddata
 
 root = sys.argv[1] # directory + root for data files
 xpar = sys.argv[2] # x-axis parameter
-xmin = float(sys.argv[3]) # minimum value of x parameter
-xmax = float(sys.argv[4]) # maximum value of x parameter
-ypar = sys.argv[5] # y-axis parameter
-ymin = float(sys.argv[6]) # minimum value of y parameter
-ymax = float(sys.argv[7]) # maximum value of y parameter
+#xmin = float(sys.argv[3]) # minimum value of x parameter
+#xmax = float(sys.argv[4]) # maximum value of x parameter
+ypar = sys.argv[3] # y-axis parameter
+#ymin = float(sys.argv[6]) # minimum value of y parameter
+#ymax = float(sys.argv[7]) # maximum value of y parameter
+ndiv = float(sys.argv[4])
 
 params_fname = root + ".paramnames"
 samples_fname = root + "posterior.txt"
+constraints_fname = root + "parameter_constraints.txt"
 
 paramnames = []
 f = open(params_fname, 'r')
@@ -32,7 +36,23 @@ f.close()
 
 samples = np.array(samples)
 
-ndiv = 40.0
+f = open(constraints_fname, 'r')
+for line in f:
+    vals = line.split()
+    if xpar in vals[0]:
+        i = vals[2].find("+-")
+        xmean = float(vals[2][:i])
+        xsd = float(vals[2][i+2:])
+        xmin = max(0.0, xmean - 3.0*xsd)
+        xmax = xmean + 3.0*xsd
+    elif ypar in vals[0]:
+        i = vals[2].find("+-")
+        ymean = float(vals[2][:i])
+        ysd = float(vals[2][i+2:])
+        ymin = max(0.0, ymean - 3.0*ysd)
+        ymax = ymean + 3.0*ysd
+
+#ndiv = 40.0
 xbin = (xmax - xmin) / ndiv
 ybin = (ymax - ymin) / ndiv
 x_grid, y_grid = np.meshgrid(np.arange(xmin, xmax, xbin), np.arange(ymin, ymax, ybin))
@@ -65,8 +85,8 @@ maxvalue = np.max(prob)
 cplot = plt.contourf(x_grid, y_grid, prob, levels=levels)#, 50, cmap="RdBu")#, vmin=0, vmax=1)
 
 cbar = plt.colorbar(cplot)
-#ax_c.set_label("$2 \Delta ln(L)$", size=20)
-#ax_c.set_ticks([0, 1.0, 3.0, 9.0, 16.0, 25.0, 1e6])
+#cbar.set_label("$2 \Delta ln(L)$", size=20)
+#cbar.set_ticks([0, 1.0, 3.0, 9.0, 16.0, 25.0, 1e6])
 
 #plt.xlim(xmin - xbin, xmax + xbin)
 #plt.ylim(ymin - ybin, ymax + ybin)
@@ -75,4 +95,4 @@ plt.ylabel(ypar, size=20)
 plt.tight_layout()
 
 plt.show()
-#plt.savefig("log_reg_nl.png")
+#plt.savefig("name.png")
--- python/run_plot_posterior.sh
+++ python/run_plot_posterior.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python plot_posterior.py ../completed_runs/standard_LCDM_mn_build/LCDM_planck omch2 0.1128 0.1261 h 0.65 0.71
+#python plot_posterior.py ../completed_runs/standard_LCDM_mn_build/LCDM_planck omch2 0.1128 0.1261 h 0.65 0.71
+python plot_posterior.py ../completed_runs/standard_LCDM_mn_build/LCDM_planck omch2 h 40
